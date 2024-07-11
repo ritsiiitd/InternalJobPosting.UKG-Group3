fc4528448f1d31558ebc79a3f336f1378ce9b4ef
ritsiiitd <ritick20460@iiitd.ac.in>
2024-07-11 10:17:51 +0530

    splitting into 2 projects

---
+++ ApplicationIJP/src/main/java/com/ukgG3/Application/IjpApplication.java
@@ -0,0 +1,13 @@
+package com.ukgG3.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjpApplication.class, args);
+	}
+
+}
---
+++ JobPostIJP/src/main/java/com/ukgG3/JobPosting/JobPosting.java
@@ -0,0 +1,11 @@
+package com.ukgG3.JobPosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPosting {
+	public static void main(String[] args) {
+		SpringApplication.run(JobPosting.class, args);
+	}
+}
