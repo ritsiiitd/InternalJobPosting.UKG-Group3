8c205d32a051567146a10bc40d701c7f383c1919
Pallav Singla <pallav.singla@ukg.com>
2024-07-15 10:23:29 +0530

    Open feign and Security

--- gatewayserver/src/main/java/com/ukgG3/gatewayserver/configuaration/SecurityConfiguration.java
+++ gatewayserver/src/main/java/com/ukgG3/gatewayserver/configuaration/SecurityConfiguration.java
@@ -17,12 +17,16 @@ public class SecurityConfiguration {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
         httpSecurity.authorizeExchange(exchange -> exchange
 //                .pathMatchers(HttpMethod.GET).permitAll()
-                .pathMatchers(HttpMethod.GET, "/application/api/applications/{id}").hasRole("EMPLOYEE")
+//                .pathMatchers(HttpMethod.GET, "/application/api/applications/{id}").hasRole("MANAGER")
                 .pathMatchers(HttpMethod.GET,"jobposting/api/jobPostings/getAll").hasRole("EMPLOYEE")
+                .pathMatchers(HttpMethod.POST, "application/api/aplications/createApplication").hasRole("EMPLOYEE")
                 .pathMatchers("/application/api/applications/**").hasRole("HR")
                 .pathMatchers("/jobposting/api/jobPostings/**").hasRole("HR")
-                        .pathMatchers(HttpMethod.GET,"/jobposting/api/jobPostings/**").hasRole("MANGER")
-                        .pathMatchers(HttpMethod.GET, "/application/api/applications/getAllApplications").hasRole("Manager")
+                .pathMatchers(HttpMethod.GET,"/jobposting/api/jobPostings/**").hasRole("MANGER")
+                .pathMatchers(HttpMethod.GET, "/application/api/applications/**").hasRole("MANAGER")
+                .pathMatchers(HttpMethod.PUT, "/application/api/applications/{id}/verifyByManager").hasRole("MANAGER")
+                .pathMatchers(HttpMethod.PUT, "/application/api/applications/{id}/verifyByManagerRejected").hasRole("MANAGER")
+                .pathMatchers(HttpMethod.GET, "application/api/applications/{id}").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
 
         ).oauth2ResourceServer(config -> config.jwt(Customizer.withDefaults()));
 
