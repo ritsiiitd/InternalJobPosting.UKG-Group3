1800cd5e04b8f591492015dc76a4376616f9fc3a
Pallav Singla <pallav.singla@ukg.com>
2024-07-16 09:30:32 +0530

    Security running

--- gatewayserver/src/main/java/com/ukgG3/gatewayserver/configuaration/SecurityConfiguration.java
+++ gatewayserver/src/main/java/com/ukgG3/gatewayserver/configuaration/SecurityConfiguration.java
@@ -3,11 +3,17 @@ package com.ukgG3.gatewayserver.configuaration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -18,23 +24,37 @@ public class SecurityConfiguration {
         httpSecurity.authorizeExchange(exchange -> exchange
 //                .pathMatchers(HttpMethod.GET).permitAll()
 //                .pathMatchers(HttpMethod.GET, "/application/api/applications/{id}").hasRole("MANAGER")
-                .pathMatchers(HttpMethod.GET,"jobposting/api/jobPostings/getAll").hasRole("EMPLOYEE")
-                .pathMatchers(HttpMethod.POST, "application/api/aplications/createApplication").hasRole("EMPLOYEE")
-                .pathMatchers("/application/api/applications/**").hasRole("HR")
-                .pathMatchers("/jobposting/api/jobPostings/**").hasRole("HR")
-                .pathMatchers(HttpMethod.GET,"/jobposting/api/jobPostings/**").hasRole("MANGER")
-                .pathMatchers(HttpMethod.GET, "/application/api/applications/**").hasRole("MANAGER")
+                //JOB POSTING API'S
+                .pathMatchers(HttpMethod.GET,"jobposting/api/jobPostings/getAllJobs").hasRole("EMPLOYEE")
+                .pathMatchers(HttpMethod.GET,"jobposting/api/jobPostings/getJobById").hasRole("EMPLOYEE")
+                .pathMatchers(HttpMethod.POST,"jobposting/api/jobPostings/**").hasRole("HR")
+                .pathMatchers(HttpMethod.DELETE,"jobposting/api/jobPostings/deleteJob").hasRole("HR")
+                .pathMatchers(HttpMethod.PUT,"jobposting/api/jobPostings/closeJobPosting").hasRole("HR")
+
+                //APPLICATIONS APIS
+
+
+                .pathMatchers(HttpMethod.GET,"/application/api/applications/**").hasAnyRole("HR", "MANAGER")
+                .pathMatchers(HttpMethod.POST, "/application/api/applications/createApplications").hasAnyRole("EMPLOYEE","HR", "MANAGER")
+                .pathMatchers(HttpMethod.DELETE,"/application/api/applications/{id}").hasRole("HR")
                 .pathMatchers(HttpMethod.PUT, "/application/api/applications/{id}/verifyByManager").hasRole("MANAGER")
                 .pathMatchers(HttpMethod.PUT, "/application/api/applications/{id}/verifyByManagerRejected").hasRole("MANAGER")
-                .pathMatchers(HttpMethod.GET, "application/api/applications/{id}").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
 
-        ).oauth2ResourceServer(config -> config.jwt(Customizer.withDefaults()));
+                //EMployees APi
+//                        .pathMatchers(HttpMethod.GET,"/jobposting/api/employees/getAll").permitAll()
+                .pathMatchers(HttpMethod.GET, "/jobposting/api/employees/**").hasRole("HR")
+                .pathMatchers(HttpMethod.PUT, "/jobposting/api/employees/getById").hasRole("EMPLOYEE")
+        ).oauth2ResourceServer(config -> config.jwt(spec -> spec.jwtAuthenticationConverter(grantedAuthorityExtractor())));
 
         httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
 
         return httpSecurity.build();
     }
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthorityExtractor(){
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+
 }
-
-
 
---
+++ message/src/main/java/com/example/message/function/MessageFunction.java
@@ -0,0 +1,9 @@
+package com.example.message.function;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageFunction {
+
+    public Function<(AccountsM)->>email()
+}
